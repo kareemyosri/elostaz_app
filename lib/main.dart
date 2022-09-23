a83f46e6426dd2296d03ba7363952511c5f1a44f
Hossam Ahmed <hossamahmed79766@gmail.com>
2022-09-23 22:42:59 +0200

    modified:   lib/main.dart 	modified:   lib/modules/login/login_screen.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:elostaz_app/share/observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'modules/login/login_screen.dart';
 import 'share/utils/custom_theme.dart';
 
-void main() {
+void main() async {
   Bloc.observer = AppBlocObserver();
-
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:elostaz_app/modules/login/login_cubit.dart';
+import 'package:elostaz_app/repo/auth.dart';
 import 'package:elostaz_app/share/components/custom_toast_message.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -17,95 +18,103 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final AuthRepo authRepo = AuthRepo();
+    return BlocProvider<LoginCubit>(
+      create: (context) => LoginCubit(authRepo),
+      child: const LoginScreenBody(),
+    );
+  }
+}
+
+class LoginScreenBody extends StatelessWidget {
+  const LoginScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     ScreenUtils().init(context);
-    return BlocListener<LoginCubit, LoginState>(
-      listener: (context, state) {
-        if (state.status == FormzStatus.submissionFailure) {
-          showtoast(
-            text: state.errorMessage ?? 'Authentication Failure',
-            state: ToastStates.ERROR,
-          );
-        }
-      },
-      child: Scaffold(
-          body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            BackButtonLS(),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: getProportionateScreenWidth(16),
-                ),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          'Log In Continue!',
-                          style:
-                              Theme.of(context).textTheme.headline3!.copyWith(
-                                    fontWeight: FontWeight.bold,
-                                  ),
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          BackButtonLS(),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(16),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'Log In Continue!',
+                        style: Theme.of(context).textTheme.headline3!.copyWith(
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  const SocialMediaLogin(
+                    method: 'Login',
+                  ),
+                  const Spacer(),
+                  const OrRow(),
+                  const Spacer(),
+                  const TextFields(),
+                  const Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Text(
+                        'Forgot Password?',
+                        style: TextStyle(
+                          color: kPrimaryGreen,
+                          fontWeight: FontWeight.bold,
                         ),
-                      ],
-                    ),
-                    const Spacer(),
-                    const SocialMediaLogin(
-                      method: 'Login',
-                    ),
-                    const Spacer(),
-                    const OrRow(),
-                    const Spacer(),
-                    const TextFields(),
-                    const Spacer(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: const [
-                        Text(
-                          'Forgot Password?',
-                          style: TextStyle(
-                            color: kPrimaryGreen,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const Spacer(),
-                    BlocBuilder<LoginCubit, LoginState>(
-                      builder: (context, state) {
-                        return ElevatedButton(
-                          onPressed: () => state.status.isValidated
-                              ? context
-                                  .read<LoginCubit>()
-                                  .logInWithCredentials()
-                              : null,
-                          child: state.status.isSubmissionInProgress
-                              ? const CircularProgressIndicator()
-                              : const Text('Login'),
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  BlocConsumer<LoginCubit, LoginState>(
+                    listener: (context, state) {
+                      if (state.status == FormzStatus.submissionFailure) {
+                        showtoast(
+                          text: state.errorMessage ?? 'Authentication Failure',
+                          state: ToastStates.ERROR,
                         );
-                      },
-                    ),
-                    const Spacer(
-                      flex: 4,
-                    ),
-                    OptionButton(
-                      desc: 'Don\'t have an account? ',
-                      method: 'Sign Up',
-                      onPressHandler: () {
-                        // Navigator.of(context).pushNamed(SignupScreen.routeName);
-                      },
-                    ),
-                    const Spacer(),
-                  ],
-                ),
+                      }
+                    },
+                    builder: (context, state) {
+                      return ElevatedButton(
+                        onPressed: () => state.status.isValidated
+                            ? context.read<LoginCubit>().logInWithCredentials()
+                            : null,
+                        child: state.status.isSubmissionInProgress
+                            ? const CircularProgressIndicator()
+                            : const Text('Login'),
+                      );
+                    },
+                  ),
+                  const Spacer(
+                    flex: 4,
+                  ),
+                  OptionButton(
+                    desc: 'Don\'t have an account? ',
+                    method: 'Sign Up',
+                    onPressHandler: () {
+                      // Navigator.of(context).pushNamed(SignupScreen.routeName);
+                    },
+                  ),
+                  const Spacer(),
+                ],
               ),
             ),
-          ],
-        ),
-      )),
-    );
+          ),
+        ],
+      ),
+    ));
   }
 }
 
