10355e3b81d2ee8f23c39bfc5036c020a32c1fe2
Hossam Ahmed <hossamahmed79766@gmail.com>
2022-09-26 22:14:56 +0200

    modified:   lib/modules/Profile/cubit/user_cubit.dart

--- lib/modules/Profile/cubit/user_cubit.dart
+++ lib/modules/Profile/cubit/user_cubit.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:elostaz_app/models/user/userModel.dart';
 import 'package:elostaz_app/repo/db.dart';
 import 'package:elostaz_app/repo/storage.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 part 'user_state.dart';
@@ -23,112 +25,29 @@ class UserCubit extends Cubit<UserDataState> {
 
   final DatabaseRepo _databaseRepo;
   final StorageRepo _storageRepo;
+
+  late UserModel user;
+
   StreamSubscription<UserModel>? streamSubscription;
 
   StreamSubscription<UserModel> monitorUserData() {
-<<<<<<< HEAD
-    return streamSubscription = _databaseRepo.userData().listen((user) {
-      emit(UserDataLoading());
-      emitUserDataLoaded(user);
-=======
     return streamSubscription = _databaseRepo.userData().listen((newuser) {
-      emit( UserDataLoading());
-      emit( UserDataLoaded());
+      emit(UserDataLoading());
+      emit(UserDataLoaded());
 
       //emitUserDataLoaded(user);
-      user=newuser;
->>>>>>> 454ad92ef38ba0267bdd23d70cd7ad03c465903e
+      user = newuser;
     }, onError: (error) => emitUserDataNotLoaded(error));
   }
 
   //void emitUserDataLoaded(user) => emit(UserDataLoaded(user: user));
   void emitUserDataNotLoaded(e) => emit(UserDataNotLoaded(e: e));
 
-  Future<File?> getProfileImage() async {
-    File pickedFilePath;
-    final ImagePicker picker = ImagePicker();
-
-    var pickedFile = await picker.pickImage(source: ImageSource.gallery);
-    if (pickedFile != null) {
-      emit(const ProfileImagePickedSuccessState());
-      pickedFilePath = File(pickedFile.path);
-      return pickedFilePath;
-    } else {
-      emit(const ProfileImageErrorState());
-    }
-    return null;
-  }
-
-  // void UploadProfileImage({
-  //   String? name,
-  //   required String email,
-  //   String? phone,
-  //   required String image,
-  //   required String uid,
-  // }) {
-  //   emit(const UserUpdateLoadingState());
-
-  //   firebase_storage.FirebaseStorage.instance
-  //       .ref()
-  //       .child('users/${Uri.file(ProfileImage!.path).pathSegments.last}')
-  //       .putFile(ProfileImage!)
-  //       .then((value) {
-  //     value.ref.getDownloadURL().then((value) {
-  //       print(value);
-  //       updateUser(
-  //         name: name,
-  //         phone: phone,
-  //         email: email,
-  //         image: image,
-  //         uid: uid,
-  //       );
-  //     }).catchError((error) {
-  //       print(error.toString());
-  //       emit(const UploadProfileImageErrorState());
-  //     });
-  //   }).catchError((error) {
-  //     print(error.toString());
-
-  //     emit(const UploadProfileImageErrorState());
-  //   });
-  // }
-
-  // updateUser({
-  //   String? name,
-  //   required String email,
-  //   required String uid,
-  //   String? phone,
-  //   String? image,
-  // }) {
-  //   UserModel model = UserModel(
-  //     name: name,
-  //     phone: phone,
-  //     email: email,
-  //     image: image,
-  //     uid: uid,
-  //   );
-  //   FirebaseFirestore.instance
-  //       .collection('users')
-  //       .doc(uid)
-  //       .update(model.toDocument())
-  //       .then((value) {
-  //     emit(const UserUpdatedSuccessState());
-  //   }).catchError((error) {
-  //     print(error.toString());
-  //     emit(const UserUpdateErrorState());
-  //   });
-  // }
-
   @override
   Future<void> close() {
     streamSubscription!.cancel();
     return super.close();
   }
-<<<<<<< HEAD
-=======
-
-
-  late UserModel user;
 
   ImagePicker picker = ImagePicker();
 
@@ -136,80 +55,68 @@ class UserCubit extends Cubit<UserDataState> {
   Future<void> getProfileImage() async {
     var pickedFile = await picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
-      ProfileImage= File(pickedFile.path);
-      emit(ProfileImagePickedSuccessState());
+      ProfileImage = File(pickedFile.path);
+      emit(const ProfileImagePickedSuccessState());
     } else {
-      emit(ProfileImageErrorState());
+      emit(const ProfileImageErrorState());
     }
   }
-
 
   void UploadProfileImage({
     String? name,
     required String email,
     String? phone,
     required String uid,
+  }) {
+    emit(const UserUpdateLoadingState());
 
-  })
-  {
-    emit(UserUpdateLoadingState());
-
-    firebase_storage.FirebaseStorage.instance.ref().child('users/${Uri.file(ProfileImage!.path).pathSegments.last}')
+    firebase_storage.FirebaseStorage.instance
+        .ref()
+        .child('users/${Uri.file(ProfileImage!.path).pathSegments.last}')
         .putFile(ProfileImage!)
         .then((value) {
-      value.ref.getDownloadURL()
-          .then((value) {
+      value.ref.getDownloadURL().then((value) {
         print(value);
         updateUser(
           name: name,
           phone: phone,
           email: email,
-          uid:uid,
+          uid: uid,
         );
-
-
-      })
-          .catchError((error){
+      }).catchError((error) {
         print(error.toString());
-        emit(UploadProfileImageErrorState());
+        emit(const UploadProfileImageErrorState());
       });
-    })
-
-        .catchError((error){
+    }).catchError((error) {
       print(error.toString());
 
-      emit(UploadProfileImageErrorState());
-
+      emit(const UploadProfileImageErrorState());
     });
-
   }
 
   updateUser({
-     String? name,
+    String? name,
     required String email,
     required String uid,
     String? phone,
     String? image,
-
-  })
-  {
-    UserModel model =UserModel(
+  }) {
+    UserModel model = UserModel(
       name: name,
       phone: phone,
       email: email,
       image: image,
       uid: uid,
     );
-    FirebaseFirestore.instance.collection('users').doc(uid).update(model.toDocument())
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(uid)
+        .update(model.toDocument())
         .then((value) {
-          emit(UserUpdatedSuccessState());
-    })
-        .catchError((error){
+      emit(const UserUpdatedSuccessState());
+    }).catchError((error) {
       print(error.toString());
-      emit(UserUpdateErrorState());
+      emit(const UserUpdateErrorState());
     });
-
   }
-
->>>>>>> 454ad92ef38ba0267bdd23d70cd7ad03c465903e
 }
